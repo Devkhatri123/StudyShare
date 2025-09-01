9c065dc7e6457059b2e8a37d9925f251dcb65b50
Dev khatri <khatridev318@gmail.com>
2025-09-01 18:33:56 +0500

    Pushing to production on vercel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/study-share"
+  base:process.env.VITE_API_URL || "/StudyShare"
 })
